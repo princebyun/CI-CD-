3784736731108743ed605d910b7e35d241556347
princebyun <princebyun@gmail.com>
2024-03-13 14:49:28 +0900

    ci/cd 배포연습 커밋

---
+++ src/test/java/Jenkins/CICD/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package Jenkins.CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
